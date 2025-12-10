2b2a30c74020c4e63e314a40aeb28f93687bdd39
iamsukanta <sukantabalacste2016@gmail.com>
2025-12-11 00:48:21 +0100

    footer file updated

--- components/footer.jsx
+++ components/footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <footer className="border-t border-gray-800/50 py-8 px-6 max-w-7xl mx-auto item-center flex flex-col md:flex-row items-center justify-between gap-4">
       <div className="flex items-center gap-6 text-sm text-gray-400">
-        &copy; {new Date().getFullYear()} Spott.com. All rights reserved.
+        <p>&copy; {new Date().getFullYear()} Spott.com. All rights reserved.</p>
       </div>
     </footer>
   );
